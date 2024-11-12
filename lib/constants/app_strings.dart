2d21607ce69b08e0e3f9e6228c5b7e2754f23784
ayomi <ayomilotunde02@gmail.com>
2024-11-12 05:33:40 +0100

    removed redundant files

--- lib/constants/app_strings.dart
+++
@@ -1,27 +0,0 @@
-class AppStrings {
-  static const appName = "KONNECT";
-  static const welcome = "Welcome to";
-  static const hello = "Hello ";
-  static const amount = "N7,600.00  ";
-
-  static const forgotPassword = "Forget Password? ";
-  static const clickHere = "Click here";
-  static const tobi = "Tobi";
-  static const bookARide = "Book a Ride";
-  static const noRideHistory = "No ride history";
-  static const departureDate = "Date of departure";
-  static const tripMode = "Trip Mode";
-  static const oneWay = "One way trip";
-  static const roundTrip = "Round Trip";
-  static const disclaimer = "Disclaimer";
-  static const caution = "Caution";
-  static const logOut = "Logout";
-  static const logOutContent = "Are you sure you want to Logout?";
-  static const cautionDetails = "Are you sure the details are correct";
-  static const review =
-      "I had a fantastic experience with my driver, Ade, during my ride with Konnect Ride. From the moment he arrived, I was impressed with his professionalism and courteous demeanor. He was on time, and his car was spotless and well-maintained, which made the ride very comfortable.";
-  static const disclaimerContent = "By accepting, you agree to cover any additional charges, penalties, or legal liabilities that may result from the third party’s actions. You are confirming the third party is well known or trusted by yourself.";
-  static const addCarDisclaimerContent = "By accepting, you confirm that all document and pictures uploaded are real and belongs to you";
-  static const driverDetails =
-      "Toyota Camry Xle Departs from Obalende (2:00pm) Destination: Ibadan, final bus stop: Iwo Road Route: Obalende- Ojota- Berger- Mowe- Iwo road Driver pickup points- Ojota and Berger";
-}
